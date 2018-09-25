bc7f40a5655ecdb5c9ad06e3c27db03bf4a98db7
Kate Dubitski <kate.dubitski@gmail.com>
2018-09-25 12:59:24 -0400

    added description - solved chalange

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//new vue instance
 const app = new Vue({
+  //el = element that is mounted on the element with id app
   el: '#app',
+
+  //data is the data we are passing to the el above
   data: {
     product: 'Socks',
+    description:
+      'These Fluffy socks will keep your toes snug and warm all winter long!',
   },
 });
